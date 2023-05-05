6b29d2974cc6901b8ac2dc4067b962b5e1875475
encryptid <o3w48mbm38oyhw8b5yhaw4@email.com>
2023-05-05 10:51:12 -0700

    Updated resume

---
+++ _nuxt/static/1683308920/images/studio/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1683308920",layout:"default",error:a,serverRendered:false,routePath:"\u002Fimages\u002Fstudio",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ _nuxt/static/1683308920/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Fdev","\u002F","\u002Fabout","\u002Fresume"]})
